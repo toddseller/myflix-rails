fd0983315346daed154902c3ea5f008a8e264aa8
Todd Seller <me@toddseller.com>
2017-08-05 23:02:00 -0700

    Update post sign in route and add sign out functionality

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -24,6 +24,6 @@ class Users::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
   def after_sign_in_path_for(resource)
-    root_path
+    user_path(current_user)
   end
 end
