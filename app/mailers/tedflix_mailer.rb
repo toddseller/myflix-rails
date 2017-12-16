4183c478ee49649c3d804ebc75e34775bc2dc219
Todd Seller <me@toddseller.com>
2017-12-15 19:53:47 -0800

    update mailer method

--- app/mailers/tedflix_mailer.rb
+++ app/mailers/tedflix_mailer.rb
@@ -2,9 +2,9 @@ class TedflixMailer < ActionMailer::Base
   default from: 'Tedflix <noreply@myflix.stream>'
   layout 'mailer'
 
-  def weekly_update(fn, email)
-    @first_name = fn
-    @email = email
+  def weekly_update(options = {})
+    @first_name = [:name]
+    @email = [:email]
     mail(to: "#{@first_name} <#{@email}>", subject: "#{@first_name} we just added some shows you may like")
   end
 end
