bc339320ae37ef23570cd1232daff02eae7283e2
Todd Seller <me@toddseller.com>
2017-12-23 09:21:22 -0800

    Add minified test email

--- app/mailers/tedflix_mailer.rb
+++ app/mailers/tedflix_mailer.rb
@@ -37,4 +37,10 @@ class TedflixMailer < ActionMailer::Base
     @email = options[:email]
     mail(to: "#{@first_name} <#{@email}>", subject: "#{@first_name}, Get a little naughty with Tedflix")
   end
+
+  def test(options = {})
+    @first_name = options[:name]
+    @email = options[:email]
+    mail(to: "#{@first_name} <#{@email}>", subject: "#{@first_name} we just added some shows you may like")
+  end
 end
