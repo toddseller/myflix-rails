eed55a748b19b2b30582d48df6d06d46e7ea73a3
Todd Seller <me@toddseller.com>
2022-09-08 18:15:20 -0700

    Add new mailer

--- app/mailers/bactfja_mailer.rb
+++ app/mailers/bactfja_mailer.rb
@@ -209,5 +209,13 @@ class BactfjaMailer < ActionMailer::Base
     @uuid = options[:uuid]
     mail(to: "#{@name} <#{@email}>", subject: "#{@first_name}, Breaking News - Julian's UK Appeal Denied!")
   end
+
+  def urgent_request_and_press_release(options = {})
+    @name = options[:full_name]
+    @first_name = options[:first_name]
+    @email = options[:email]
+    @uuid = options[:uuid]
+    mail(to: "#{@name} <#{@email}>", subject: "#{@first_name}, Press Release for Upcoming Protest!")
+  end
 end
 
